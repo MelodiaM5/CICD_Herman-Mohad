15d271fd7ed67d3cff75dc20ad5a58d97f72a438
Théo Herman <hermantheo@hotmail.com>
2022-02-13 23:31:45 +0100

    change(delete and test)

--- menu-front/src/test/test_listmenus.py
+++ menu-front/src/test/test_listmenus.py
@@ -1,8 +1,9 @@
 # Third-party imports...
 from nose.tools import assert_is_not_none
-
+import unittest
 # Local imports...
 from services.list import listmenus
+from services.list import rewrite
 from constants import BASE_URL
 
 def test_request_response():
@@ -11,3 +12,13 @@ def test_request_response():
 
     # If the request is sent successfully, then I expect a response to be returned.
     assert_is_not_none(response)
+
+    def test_rewrite_ok(self):
+        json = [{'id': 1, 'name': 'Menu spécial du chef2', 'dishes': [{'id': 1, 'name': 'Bananes aux fraises'}, {'id': 2, 'name': 'Bananes flambées le retour'}]}]
+        text = rewrite(json)
+        self.assertEqual(text, '1 Menu spécial du chef2\n   Bananes aux fraises\n   Bananes flambées le retour\n')
+
+    def test_rewrite_empty(self):
+        json = []
+        text = rewrite(json)
+        self.assertEqual(text,'There is no menu yet')
